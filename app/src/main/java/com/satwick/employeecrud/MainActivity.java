e83023837364b633e385ffa1716d14445253721f
Satwick Dash <satwick1995@gmail.com>
2019-06-06 00:06:27 +0530

    Phone Number authentication implemented

--- app/src/main/java/com/satwick/employeecrud/MainActivity.java
+++ app/src/main/java/com/satwick/employeecrud/MainActivity.java
@@ -2,13 +2,62 @@ package com.satwick.employeecrud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivityTag";
+    private static final int RC_SIGN_IN = 1005;
+
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (mFirebaseUser == null) {
+            List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.PhoneBuilder().build());
+
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            try {
+                if (resultCode == RESULT_OK) {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    Log.d(TAG, "user: " + user.getDisplayName());
+                } else {
+                    Toast.makeText(this, "Sign In Failed", Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, "Sign In failed: Code - " + resultCode + ", Error - " + response.getError().toString());
+                }
+            } catch (NullPointerException e) {
+                Log.e(TAG, e.getMessage());
+            }
+        }
     }
 }
